ed8153405c6a19cd49824e7cda2107343ad17631
Rui Ribeiro <46627071+ruiasribeiro@users.noreply.github.com>
2022-10-27 21:52:34 +0100

    Add GeoC tables and respective data generation

--- src/main/java/com/oltpbenchmark/benchmarks/geoc/GeoCConfig.java
+++ src/main/java/com/oltpbenchmark/benchmarks/geoc/GeoCConfig.java
@@ -32,7 +32,9 @@ public final class GeoCConfig {
     public final static int configWhseCount = 1;
     public final static int configItemCount = 100000; // tpc-c std = 100,000
     public final static int configDistPerWhse = 10; // tpc-c std = 10
-    public final static int configCustPerDist = 3000; // tpc-c std = 3,000
+    // It should be at minimum 1000, because of the customer name generator.
+    public final static int configCustPerDist = 1000; // tpc-c std = 3,000; 
+    public final static int configIndPerCust = 10; 
 
     /**
      * An invalid item id used to rollback a new order transaction.
---
+++ src/main/java/com/oltpbenchmark/benchmarks/geoc/pojo/ShoppingCartLine.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2022 by Adriano Maior and Rui Ribeiro
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.oltpbenchmark.benchmarks.geoc.pojo;
+
+import java.sql.Timestamp;
+
+public class ShoppingCartLine {
+    public int _scl_c_id;
+    public int _scl_d_id;
+    public int _scl_w_id;
+    public int _scl_number;
+    public int _scl_i_id;
+    public int _scl_supply_w_id;
+    public Timestamp _scl_delivery_d;
+    public int _scl_quantity;
+    public float _scl_amount;
+    public String _scl_dist_info;
+}
