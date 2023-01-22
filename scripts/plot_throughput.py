3cfacb3f831a7676545faf514535167b556b217b
Rui Ribeiro <46627071+ruiasribeiro@users.noreply.github.com>
2023-01-22 21:11:17 +0000

    Reorganise throughput script
    When loading multiple results from the same directory, the script now combines them into a single line and calculates their average; multiple directories can be passed on to the script to compare the averages between them. There's also an option to directly output to a file.

--- scripts/plot_throughput.py
+++ scripts/plot_throughput.py
@@ -1,31 +1,71 @@
+import argparse
 import csv
-import matplotlib.pyplot as plt
 import sys
 
-# Get the paths to the CSV files from the command line arguments
-if len(sys.argv) < 2:
-    print("Error: Please specify the paths to the CSV files as command line arguments.")
-    sys.exit(1)
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.typing import NDArray
 
-csv_paths = sys.argv[1:]
+# -------------------------------------
+# Arguments
+# -------------------------------------
 
-# Read in the data from each CSV file and plot it as a separate line
-for i, path in enumerate(csv_paths):
+parser = argparse.ArgumentParser()
+parser.add_argument("result_csv_paths", nargs="+")
+parser.add_argument(
+    "-o", "--output", help="Name of output file", required=False, default=None
+)
+
+args = parser.parse_args()
+
+# -------------------------------------
+# Parsing
+# -------------------------------------
+
+aggregator: dict[str, tuple[int, NDArray, NDArray]] = {}
+"""Aggregates results by type of test, the result being an average of them. The
+key is the type of test and the value is a tuple composed of (#tests, times,
+throughputs)."""
+
+for i, path in enumerate(args.result_csv_paths):
     with open(path, "r") as f:
         reader = csv.reader(f)
-        next(reader)  # Skip the first line
-        data = [(float(row[0]), float(row[2])) for row in reader]
+        next(reader)  # Skip the first line, the headers.
+        data = [(float(row[0]), float(row[1])) for row in reader]
 
-    # Unpack the data into separate lists
     times, throughputs = zip(*data)
 
-    # Plot the data
-    plt.plot(times, throughputs, label=f"File {i+1}")
+    # The name is composed of the database, benchmark and type of test ran.
+    name = ", ".join(path.split("/")[-4:-1])
 
-plt.title(", ".join(csv_paths[0].split("/")[-4:-1]))
+    if val := aggregator.get(name):
+        # Some tests count the final second, that's why sometimes the length of
+        # the arrays does not match perfectly. To avoid mismatching array
+        # shapes, we reduce the length of one of them to be as short as the
+        # shorter.
+        length = min(len(val[1]), len(throughputs))
 
-# Add a legend and show the plot
+        aggregator[name] = (
+            val[0] + 1,
+            val[1][:length],
+            val[2][:length] + np.array(throughputs)[:length],
+        )
+    else:
+        aggregator[name] = (1, np.array(times), np.array(throughputs))
+
+# -------------------------------------
+# Plotting
+# -------------------------------------
+
+for key, [count, times, throughputs] in aggregator.items():
+    plt.plot(times, throughputs / count, label=key)
+
+plt.title("Throughput chart")
 plt.legend()
 plt.xlabel("Time (s)")
 plt.ylabel("Throughput (txn/s)")
-plt.show()
+
+if args.output is not None:
+    plt.savefig(args.output, bbox_inches="tight")
+else:
+    plt.show()
